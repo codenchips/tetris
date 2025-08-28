e8679266c4bdfb69a7078db4ca1d58ce0b5d9e93
Dean Parkes <codenchips@gmail.com>
2025-08-28 10:53:21 +0100

    scores numbered

--- js/tetris.js
+++ js/tetris.js
@@ -1503,28 +1503,47 @@ async function showGameOver() {
     // save the score
     saveScore(score, name, gameElapsedTime);
     // get the scores
-    setTimeout(function () {
-      getScores(); // Refresh the scores
-    }, 1000);
+    // setTimeout(function () {
+    //   getScores(); // Refresh the scores
+    // }, 1000);
 
   }
   resetGame();
 
 }
 
-  function renderScores(scores) {        
+  function renderScores(scores, position) {        
+    let thisPosition = 0;
     if (scores) {
       $('#scoreboard-body').empty();
-      scores.forEach(function(score) {        
+      scores.slice(0, 10).forEach(function(score) {        
+        thisPosition++;
         gameElapsedTime = score.time; // Use the time from the score
         const finalMinutes = Math.floor(gameElapsedTime / 60000);
         const finalSeconds = Math.floor((gameElapsedTime % 60000) / 1000);
         const finalHundredths = Math.floor((gameElapsedTime % 1000) / 10);
         const finalTimeString = `${finalMinutes.toString().padStart(2, '0')}:${finalSeconds.toString().padStart(2, '0')}.${finalHundredths.toString().padStart(2, '0')}`;
-        const fullName = (score.firstname + ' ' + score.lastname).substring(0, 20);
+        const fullName = (score.firstname + ' ' + score.lastname).substring(0, 20);        
+        const highlighted = (position && thisPosition == position) ? "highlighted" : "";
         
-        $('#scoreboard-body').append('<tr><td>' + fullName + '</td><td>' + score.score + '</td><td>' + finalTimeString + '</td></tr>');
+        $('#scoreboard-body').append('<tr class="' + highlighted + '"><td>' + thisPosition + '</td><td>' + fullName + '</td><td>' + score.score + '&nbsp;&nbsp;</td><td align="right">' + finalTimeString + '</td></tr>');
       });
+
+      if(position > 10) {
+        // show the player's position if not in top 10
+        gameElapsedTime = scores[position - 1].time; // Use the time from the score
+        const finalMinutes = Math.floor(gameElapsedTime / 60000);
+        const finalSeconds = Math.floor((gameElapsedTime % 60000) / 1000);
+        const finalHundredths = Math.floor((gameElapsedTime % 1000) / 10);
+        const finalTimeString = `${finalMinutes.toString().padStart(2, '0')}:${finalSeconds.toString().padStart(2, '0')}.${finalHundredths.toString().padStart(2, '0')}`;
+        const fullName = (scores[position - 1].firstname + ' ' + scores[position - 1].lastname).substring(0, 20);        
+        $('#lower-scoreboard').html('<table class="uk-table  score-table">'+
+          '<tbody id="scoreboard-body">'+
+          '<tr class="highlighted"><td style="width:32px;">' + position + '</td><td style="width:60%;">' + fullName + '</td><td style="min-width:50px;">' + scores[position - 1].score + '&nbsp;&nbsp;</td><td align="right">' + finalTimeString + '</td></tr>'+
+          '</tbody>'+
+          '</table>');
+
+      }
     }     
   }
 
@@ -1550,7 +1569,9 @@ async function showGameOver() {
       type: 'POST',
       data: { score: score, name: name, time: time },
       success: function(data) {
-        console.log(data);
+        //console.log(data);
+        scores = JSON.parse(data);
+        renderScores(scores.scores, scores.position);         
       }
     });
   }
